0651d6604a98a9a96161dd5800450472bf5799a6
van <909036273@qq.com>
2023-11-18 13:13:47 +0800

    2023/11/18 首页 分类页

---
+++ src/pages/index/components/PageSkeleton.vue
@@ -0,0 +1,42 @@
+<!--
+此文件为开发者工具生成，生成时间: 2023/11/18上午11:22:05
+使用方法：
+在 C:\Users\admin\Desktop\uni-mp-template\dist\dev\mp-weixin\pages\index\index.wxml 引入模板
+
+```
+<import src="index.skeleton.wxml"/>
+<template is="skeleton" wx:if="{{loading}}" />
+```
+
+在 C:\Users\admin\Desktop\uni-mp-template\dist\dev\mp-weixin\pages\index\index.wxss 中引入样式
+```
+@import "./index.skeleton.wxss";
+```
+
+更多详细信息可以参考文档：https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html
+-->
+<template name="skeleton">
+  <view class="sk-container"></view>
+</template>
+
+<style>
+/*
+此文件为开发者工具生成，生成时间: 2023/11/18上午11:22:05
+
+在 C:\Users\admin\Desktop\uni-mp-template\dist\dev\mp-weixin\pages\index\index.wxss 中引入样式
+```
+@import "./index.skeleton.wxss";
+```
+
+更多详细信息可以参考文档：https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html
+*/
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
